5775262e299f94916826004e2b6c97cd5f18e90d
Sumit Khanna <sumit@penguindreams.org>
2020-07-29 20:27:02 -0500

    More CI, tests and cleanup

--- senseflux/webapp.py
+++ senseflux/webapp.py
@@ -14,31 +14,12 @@ def load_schema():
     return json.loads(pkg_resources.resource_string(resource_package, resource_path))
 
 
-def create_app(influxdb_client: InfluxDBClient, influxdb_database: str, api_key: str, test_config=None):
+def create_app(influxdb_client: InfluxDBClient, influxdb_database: str, api_key: str):
     # create and configure the app
     from senseflux.__main__ import log
     vegehub_schema = load_schema()
     app = Flask(__name__, instance_relative_config=True)
 
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    # )
-
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.config.from_pyfile('config.py', silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.from_mapping(test_config)
-    #
-    # # ensure the instance folder exists
-    # try:
-    #     os.makedirs(app.instance_path)
-    # except OSError:
-    #     pass
-
-    # a simple page that says hello
     @app.route('/')
     def default():
         log.info('SenseFlux Hello')
--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -20,6 +20,10 @@ class BasicTests(unittest.TestCase):
         "field3":0.080,"field4":1.824},{"created_at":"2020-07-12 15:10:37","field1":1.826,"field2":1.542,
         "field3":0.081,"field4":1.907},{"created_at":"2020-07-12 15:15:37","field1":1.827,"field2":1.544}]}"""
 
+    wrong_schema = """{"write_api_key":"TESTKey",
+        "channel_id":"TestChn", "updates":[{"created_oot":"2020-07-12 15:00:37","field1":1.827,"field2":1.541,
+        "field3":0.077,"field4":1.905}]}"""
+
     def setUp(self):
         self.influx_mock = MockInflux()
         app = create_app(self.influx_mock, 'textflux', 'TESTKey')
@@ -52,8 +56,6 @@ class BasicTests(unittest.TestCase):
 
     def test_veghub_invalid_schema(self):
         resp = self.app.post('/create/vghub', headers={'content-type': 'application/json'},
-                             data={"write_api_key": "TESTKey, broken}}{{json"})
+                             data=self.wrong_schema)
         self.assertEqual(resp.status_code, 400)
-
-    def test_veghub_invalid_content_type(self):
-        pass
+        self.assertEqual(resp.get_data().decode('UTF-8'), "Invalid VegeHub JSON: 'created_at' is a required property")
