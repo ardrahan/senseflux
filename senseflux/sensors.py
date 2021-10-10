64d6baea7205b7aad0b18b085ddf4e0e8fc76e97
ardrahan <51294887+ardrahan@users.noreply.github.com>
2021-10-10 13:45:27 -0400

    Update sensors.py

--- senseflux/sensors.py
+++ senseflux/sensors.py
@@ -6,6 +6,20 @@ def three_volt_range(voltage: float) -> float:
     """Returns (int) 0 - 100 based on ratio from 0.0 to 3.0 volts"""
     return float((voltage / 3.0) * 100)
 
+def piecewise_linear(voltage: float) -> float:
+    """https://vegetronix.com/Products/VH400/VH400-Piecewise-Curve.phtml"""
+    vwc = 0
+    if voltage <= 1.1:
+        vwc = 10.0*voltage - 1.0
+    elif voltage > 1.1 and voltage <= 1.3:  
+        vwc = 25.0*voltage - 7.5
+    elif voltage > 1.3 and voltage <= 1.82:
+        vwc = 48.08*voltage - 47.5
+    elif voltage > 1.82:
+        vwc = 26.32*voltage - 7.89
+    
+    return vwc
+  
 
 def voltage_to_temperature(voltage: float, units='C'):
     if units == 'C':
@@ -29,9 +43,10 @@ def fields_to_values(updates: [], field_map: Dict[str, str]) -> []:
                 adjval = voltage_to_temperature(v)
             elif f == 'Battery':
                 adjval = v
+            elif f == 'SoilMoisture':
+                adjval = piecewise_linear(v)
             else:
                 adjval = three_volt_range(v)
-
 
             ret_u[f] = adjval
         ret.append(ret_u)
