08d96c2dc334f0d06b07c19ed060e4636500e72a
ardrahan <51294887+ardrahan@users.noreply.github.com>
2021-10-12 17:52:52 -0400

    Update util.py

--- senseflux/util.py
+++ senseflux/util.py
@@ -2,6 +2,6 @@ import datetime
 
 
 def veghub_time_to_timestamp(created_at: str) -> int:
-    parsed = datetime.datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S%Z")
+    parsed = datetime.datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
     zoned = parsed.replace(tzinfo=datetime.timezone.utc)
     return int(zoned.timestamp())
