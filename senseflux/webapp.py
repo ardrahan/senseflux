07431000c03f7f0525fb4f5e2a49f7792714aed5
ardrahan <51294887+ardrahan@users.noreply.github.com>
2021-10-12 17:21:35 -0400

    Update webapp.py

--- senseflux/webapp.py
+++ senseflux/webapp.py
@@ -4,7 +4,7 @@ import jsonschema
 from flask import Flask, request, Response
 from influxdb_client import WritePrecision, InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
-from senseflux.sensors import fields_to_values
+from senseflux.sensors import *
 from senseflux.util import veghub_time_to_timestamp
 from jsonschema import validate
 import pkg_resources
@@ -41,21 +41,40 @@ def create_app(influxdb_client: InfluxDBClient, influxdb_database: str, api_key:
             log.warning(f'Invalid VegeHub JSON: {err.message}')
             return Response(f'Invalid VegeHub JSON: {err.message}', status=400)
 
-        channel = data['channel_id'] if 'channel_id' in data else 'default'
+        channel = data['id'] if 'id' in data else 'default'
         req_key = data['api_key']
+        
         if api_key and api_key != req_key:
             log.warning('Invalid API Key')
             return Response(status=401)
 
         lines = []
-        log.info(f"Raw updates {data['updates']}")
-        readings = fields_to_values(data['updates'], field_map)
-        for r in readings:
-            fields = ','.join([f'{k}={v}' for k, v in r.items() if k != 'created_at'])
-            timestamp = int(veghub_time_to_timestamp(r['created_at']) * 1e9)
-            msg = f'{channel} {fields} {timestamp}'
-            log.debug(f'Line:{msg}')
-            lines.append(msg)
+        log.info(f"Raw updates {data['sensors']}")
+        
+        for s in data['sensors']:  
+            slot = s['slot']
+                for r in s['samples']:
+                    t=r['t']
+                    v=r['v']
+       
+                    timestamp=int(veghub_time_to_timestamp(t) * 1e9)
+                    name=field_map[slot]
+            
+                    value = v
+                    if name == 'SoilTemperature':
+                        value = voltage_to_temperature(v)
+                    elif f == 'Battery':
+                        value = v
+                    elif f == 'SoilMoisture':
+                        value = piecewise_linear(v)
+                    else:
+                        value = three_volt_range(v)
+         
+                    msg = f'{channel} {name}={value} {timestamp}'
+                    log.debug(f'Line:{msg}')
+                    lines.append(msg)
+   
+
         log.info('Sending Data to Influx')
         write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
         result = write_api.write(bucket=influxdb_database, record=lines)
